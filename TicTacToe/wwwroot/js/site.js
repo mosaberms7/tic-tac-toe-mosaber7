310c925020db61be5203c921a1198fb8095c98e1
Mohamed Fares <mosaber7@users.campus>
2019-11-11 17:12:20 -0600

    hey

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var CurrentTurn = "X";
+ document.getElementById("turn").innerText = "It is Player" + CurrentTurn + "'s turn";
+
+var square = document.getElementsByClassName("square");
+for (var i = 0; i < square; i++)
+{
+    square[i].addEventListener('click', onclick)
+
+}
+function onclick() {
+    event.preventDefault();
+    if (!event.target.innerText) { }
+    event.target.innerText = CurrentTurn;
+    nextTurn();
+}
+}
+function nextTurn() {
+    if (CurrentTurn === "X") {
+
+        CurrentTurn = "O";
+    }
+    else {
+
+        CurrentTurn = "X";
+    }
+
+
+
+}
